008d14002bac0c45e961ac6987e0bfdce7c09edd
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2017-05-26 20:20:59 -0300

    #2 Criação da funcionalidade de incluir URL

--- RssFind/src/controller/formCadastroSites.java
+++ RssFind/src/controller/formCadastroSites.java
@@ -5,6 +5,10 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author CCE User
@@ -12,13 +16,62 @@ package controller;
 public class formCadastroSites {
 
     private view.formCadastroSites telaCadastroSites = null;
+    private sqlite.Conection banco = null;
 
     public formCadastroSites() {
         telaCadastroSites = new view.formCadastroSites(null, true);
+        banco = new sqlite.Conection();
+        ligaEventos();
+    }
+
+    private void limparTela() {
+        telaCadastroSites.edUrl.setText("");
+        telaCadastroSites.edUrl.requestFocus();
     }
 
     public void chamaTelaCadastroSites() {
+        limparTela();
         telaCadastroSites.setVisible(true);
+    }
+
+    private void gravarUrl() {
+        if (telaCadastroSites.edUrl.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Obrigatório informar URL");
+        } else {
+            banco.setInsertUrl(telaCadastroSites.edUrl.getText());
+        }
+    }
+
+    private void fecharTelaCadastroSites() {
+        telaCadastroSites.setVisible(false);
+    }
+
+    private void ligaEventos() {
+
+        telaCadastroSites.btnGravar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               gravarUrl();
+            }
+        });
+
+        telaCadastroSites.btnSair.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fecharTelaCadastroSites();
+            }
+        });
+
+        telaCadastroSites.btnLimpar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                limparTela();
+            }
+        });
+
     }
 
 }
