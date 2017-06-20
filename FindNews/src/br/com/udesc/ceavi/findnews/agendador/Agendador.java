d840de93f40b2366f35947c67373937dca92be6a
MarcosCamargo <markos_camargo@hotmail.com>
2017-06-19 23:47:59 -0300

    #20 corrigido bug

--- FindNews/src/br/com/udesc/ceavi/findnews/agendador/Agendador.java
+++ FindNews/src/br/com/udesc/ceavi/findnews/agendador/Agendador.java
@@ -6,11 +6,8 @@
 package br.com.udesc.ceavi.findnews.agendador;
 
 import java.util.Date;
-import java.util.List;
 import java.util.TimerTask;
 import br.com.udesc.ceavi.findnews.leitorrss.IRSSImpl;
-import br.com.udesc.ceavi.findnews.modelo.Noticia;
-import br.com.udesc.ceavi.findnews.leitorrss.ImailImpl;
 
 public class Agendador extends TimerTask {
 
@@ -20,14 +17,7 @@ public class Agendador extends TimerTask {
     public void run() {
         instanteAtual = new Date();
         System.out.println("Processo Executado no Horario Definido: " + instanteAtual);
-        List<Noticia> noticias = new IRSSImpl().verificaNoticiasManual();
-
-        ///Monta e envia o email
-        String msg = "Foram encontrados link interessantes\n";
-        for (Noticia noticia : noticias) {
-            msg = msg + noticia.getTitulo() + "\n" + noticia.getLink();
-        }
-        new ImailImpl().sendEmail("markos_camargo@hotmail.com", "senhadousuario", "markos_camargo@hotmail.com", msg);
+        new IRSSImpl().verificaNoticiasManual();
     }
 
 }
